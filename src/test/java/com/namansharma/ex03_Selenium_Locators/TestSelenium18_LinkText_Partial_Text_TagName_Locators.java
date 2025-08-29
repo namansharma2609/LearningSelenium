300cf051bf8da80a93dfef937b88113ede7349d8
Naman Sharma <sharmanaman26099@gmail.com>
2025-08-29 23:29:53 +0530

    Selenium Locators

--- src/test/java/com/namansharma/ex03_Selenium_Locators/TestSelenium18_LinkText_Partial_Text_TagName_Locators.java
+++ src/test/java/com/namansharma/ex03_Selenium_Locators/TestSelenium18_LinkText_Partial_Text_TagName_Locators.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestSelenium18_LinkText_Partial_Text_TagName_Locators {
@@ -13,7 +14,7 @@ public class TestSelenium18_LinkText_Partial_Text_TagName_Locators {
     @Description("Open the trial page for app.vwo website")
     @Owner("Naman Sharma")
     @Test
-    public void test_start_trial() {
+    public void vwo_free_trail_error_verify () {
 
         //Step 1 - Open the website app.vwo.com
         WebDriver driver = new ChromeDriver();
@@ -38,7 +39,7 @@ public class TestSelenium18_LinkText_Partial_Text_TagName_Locators {
         //    required="">
 
         WebElement business_email_section = driver.findElement(By.name("email"));
-        business_email_section.sendKeys("abc@testdata.com");
+        business_email_section.sendKeys("abc@testdata");
 
 
         // Step 4 - Click on the Radio Button to Accept to VWO's
@@ -52,6 +53,31 @@ public class TestSelenium18_LinkText_Partial_Text_TagName_Locators {
         WebElement checkbox_privacy_policy = driver.findElement(By.id("page-free-trial-step1-cu-gdpr-consent-checkbox"));
         checkbox_privacy_policy.click();
 
+        // Step 5 - Click on Create a Free Trial Account to create account
+        // <button type="submit"
+        // class="button W(100%) btn-modal-form-submit"
+        // data-qa="page-su-submit"
+        // fdprocessedid="sdb3ig">Create a Free Trial Account
+        // </button>
 
+        WebElement free_trial_account_bttn = driver.findElement(By.tagName("button"));
+        free_trial_account_bttn.click();
+
+
+        //Step 6- Getting error for entering wrong email
+        //<div class="C(--color-red) Fz(--font-size-12) Trsp(--Op) Trsdu(0.15s)
+        // Op(0) invalid-input+Op(1) invalid-reason">The email address you entered is incorrect.</div>
+
+        WebElement wrong_email_error = driver.findElement(By.className("invalid-reason"));
+        System.out.println("The email address you entered is incorrect.");
+        Assert.assertEquals(wrong_email_error.getText(),"The email address you entered is incorrect.");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        driver.quit();
     }
 }
